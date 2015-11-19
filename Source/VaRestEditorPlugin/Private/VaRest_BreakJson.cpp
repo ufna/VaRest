981a72a24a6ec866220a9a53d0f1581c7c6b4148
Unktomi <christopher.d.oliver@gmail.com>
2015-11-19 14:23:42 -0800

    fixed bug where multiple break json nodes couldn't have the same field names

--- Source/VaRestEditorPlugin/Private/VaRest_BreakJson.cpp
+++ Source/VaRestEditorPlugin/Private/VaRest_BreakJson.cpp
@@ -1,8 +1,15 @@
-// Copyright 2015 Vladimir Alyamkin. All Rights Reserved.
-// Original code by https://github.com/unktomi
-
 #include "VaRestEditorPluginPrivatePCH.h"
+#include "KismetCompiler.h"
 #include "VaRest_BreakJson.h"
+#include "EditorCategoryUtils.h"
+#include "EdGraph/EdGraph.h"
+#include "EdGraph/EdGraphNodeUtils.h" // for FNodeTextCache
+#include "EdGraphSchema_K2.h"
+#include "BlueprintNodeSpawner.h"
+#include "BlueprintActionDatabaseRegistrar.h"
+#include "BlueprintFieldNodeSpawner.h"
+#include "EditorCategoryUtils.h"
+#include "BlueprintActionFilter.h"
 
 #define LOCTEXT_NAMESPACE "VaRest_BreakJson"
 
@@ -18,7 +25,6 @@ public:
 	virtual void Compile(FKismetFunctionContext& Context, UEdGraphNode* Node) override
 	{
 		UEdGraphPin* InputPin = NULL;
-
 		for (int32 PinIndex = 0; PinIndex < Node->Pins.Num(); ++PinIndex)
 		{
 			UEdGraphPin* Pin = Node->Pins[PinIndex];
@@ -28,16 +34,13 @@ public:
 				break;
 			}
 		}
-
 		UEdGraphPin *InNet = FEdGraphUtilities::GetNetFromPin(InputPin);
 		UClass *Class = Cast<UClass>(StaticLoadObject(UClass::StaticClass(), NULL, TEXT("class'VaRestPlugin.VaRestJsonObject'")));
-
 		FBPTerminal **SourceTerm = Context.NetMap.Find(InNet);
 		if (SourceTerm == nullptr)
 		{
 			return;
 		}
-
 		for (int32 PinIndex = 0; PinIndex < Node->Pins.Num(); ++PinIndex)
 		{
 			UEdGraphPin* Pin = Node->Pins[PinIndex];
@@ -47,23 +50,17 @@ public:
 				{
 					continue;
 				}
-
 				FBPTerminal **Target = Context.NetMap.Find(Pin);
-
 				const FString &FieldName = Pin->PinName;
 				const FString &FieldType = Pin->PinType.PinCategory;
-
 				FBPTerminal* FieldNameTerm = Context.CreateLocalTerminal(ETerminalSpecification::TS_Literal);
 				FieldNameTerm->Type.PinCategory = CompilerContext.GetSchema()->PC_String;
 				FieldNameTerm->Source = Pin;
 				FieldNameTerm->Name = FieldName;
 				FieldNameTerm->TextLiteral = FText::FromString(FieldName);
-
  				FBlueprintCompiledStatement& Statement = Context.AppendStatementForNode(Node);
 				FName FunctionName;
-
 				bool bIsArray = Pin->PinType.bIsArray;
-
 				if (FieldType == CompilerContext.GetSchema()->PC_Boolean)
 				{
 					FunctionName = bIsArray ? TEXT("GetBoolArrayField") : TEXT("GetBoolField");
@@ -98,7 +95,7 @@ public:
 
 	FBPTerminal* RegisterInputTerm(FKismetFunctionContext& Context, UVaRest_BreakJson* Node)
 	{
-		// Find input pin
+		//Find input pin
 		UEdGraphPin* InputPin = NULL;
 		for (int32 PinIndex = 0; PinIndex < Node->Pins.Num(); ++PinIndex)
 		{
@@ -111,25 +108,22 @@ public:
 		}
 		check(NULL != InputPin);
 
-		// Find structure source net
+		//Find structure source net
 		UEdGraphPin* Net = FEdGraphUtilities::GetNetFromPin(InputPin);
 		FBPTerminal **TermPtr = Context.NetMap.Find(Net);
-
 		if (!TermPtr)
 		{
 			FBPTerminal *Term = Context.CreateLocalTerminalFromPinAutoChooseScope(Net, Context.NetNameMap->MakeValidName(Net));
-
 			Context.NetMap.Add(Net, Term);
-
 			return Term;
 		}
-
 		return *TermPtr;
 	}
 
 	void RegisterOutputTerm(FKismetFunctionContext& Context, UEdGraphPin* OutputPin, FBPTerminal* ContextTerm)
 	{
-		FBPTerminal *Term = Context.CreateLocalTerminalFromPinAutoChooseScope(OutputPin, OutputPin->PinName);
+		FBPTerminal *Term = Context.CreateLocalTerminalFromPinAutoChooseScope(OutputPin, Context.NetNameMap->MakeValidName(OutputPin));
+		Term->Context = ContextTerm;
 		Context.NetMap.Add(OutputPin, Term);
 	}
 
@@ -137,7 +131,6 @@ public:
 	{
 		UVaRest_BreakJson* Node = Cast<UVaRest_BreakJson>(InNode);
 		FNodeHandlingFunctor::RegisterNets(Context, Node);
-
 		check(NULL != Node);
 
 		if (FBPTerminal* StructContextTerm = RegisterInputTerm(Context, Node))
@@ -154,9 +147,7 @@ public:
 	}
 };
 
-/**
- * Main node class
- */
+
 UVaRest_BreakJson::UVaRest_BreakJson(const FObjectInitializer &ObjectInitializer)
 	: Super(ObjectInitializer)
 {
@@ -170,12 +161,9 @@ FNodeHandlingFunctor* UVaRest_BreakJson::CreateNodeHandler(class FKismetCompiler
 void UVaRest_BreakJson::AllocateDefaultPins()
 {
 	const UEdGraphSchema_K2* K2Schema = GetDefault<UEdGraphSchema_K2>();
-
 	UClass *Class = Cast<UClass>(StaticLoadObject(UClass::StaticClass(), NULL, TEXT("class'VaRestPlugin.VaRestJsonObject'")));
 	UEdGraphPin* Pin = CreatePin(EGPD_Input, K2Schema->PC_Object, TEXT(""), Class, false, false, TEXT("Target"));
-
 	K2Schema->SetPinDefaultValueBasedOnType(Pin);
-
 	CreateProjectionPins(Pin);
 }
 
@@ -186,8 +174,8 @@ FLinearColor UVaRest_BreakJson::GetNodeTitleColor() const
 
 void UVaRest_BreakJson::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
 {
-	bool bIsDirty = false;
 
+	bool bIsDirty = false;
 	FName PropertyName = (PropertyChangedEvent.Property != NULL) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
 	if (true || PropertyName == TEXT("Outputs"))
 	{
@@ -199,9 +187,11 @@ void UVaRest_BreakJson::PostEditChangeProperty(struct FPropertyChangedEvent& Pro
 		ReconstructNode();
 		GetGraph()->NotifyGraphChanged();
 	}
-
 	Super::PostEditChangeProperty(PropertyChangedEvent);
 }
+// End UEdGraphNode interface.
+
+// Begin UK2Node interface
 
 void UVaRest_BreakJson::GetMenuActions(FBlueprintActionDatabaseRegistrar& ActionRegistrar) const
 {
@@ -210,7 +200,6 @@ void UVaRest_BreakJson::GetMenuActions(FBlueprintActionDatabaseRegistrar& Action
 	// mutated (or removed)... here we use the node's class (so if the node 
 	// type disappears, then the action should go with it)
 	UClass* ActionKey = GetClass();
-
 	// to keep from needlessly instantiating a UBlueprintNodeSpawner, first   
 	// check to make sure that the registrar is looking for actions of this type
 	// (could be regenerating actions for a specific asset, and therefore the 
@@ -227,7 +216,6 @@ void UVaRest_BreakJson::GetMenuActions(FBlueprintActionDatabaseRegistrar& Action
 FText UVaRest_BreakJson::GetMenuCategory() const
 {
 	static FNodeTextCache CachedCategory;
-
 	if (CachedCategory.IsOutOfDate(this))
 	{
 		// FText::Format() is slow, so we cache this to save on performance
@@ -235,37 +223,33 @@ FText UVaRest_BreakJson::GetMenuCategory() const
 	}
 	return CachedCategory;
 }
+// End UK2Node interface.
+
 
 void UVaRest_BreakJson::CreateProjectionPins(UEdGraphPin *Source)
 {
 	const UEdGraphSchema_K2* K2Schema = GetDefault<UEdGraphSchema_K2>();
 	UClass *Class = Cast<UClass>(StaticLoadObject(UClass::StaticClass(), NULL, TEXT("class'VaRestPlugin.VaRestJsonObject'")));
-
 	for (TArray<FVaRest_NamedType>::TIterator it(Outputs); it; ++it)
 	{
 		FString Type;
 		UObject *Subtype = nullptr;
 		FString FieldName = (*it).Name;
-
 		switch ((*it).Type) {
-			case EVaRest_JsonType::JSON_Bool:
-				Type = K2Schema->PC_Boolean;
-				break;
-
-			case EVaRest_JsonType::JSON_Number:
-				Type = K2Schema->PC_Float;
-				break;
-
-			case EVaRest_JsonType::JSON_String:
-				Type = K2Schema->PC_String;
-				break;
-
-			case EVaRest_JsonType::JSON_Object:
-				Type = K2Schema->PC_Object;
-				Subtype = Class;
-				break;
+		case EVaRest_JsonType::JSON_Bool:
+			Type = K2Schema->PC_Boolean;
+			break;
+		case EVaRest_JsonType::JSON_Number:
+			Type = K2Schema->PC_Float;
+			break;
+		case EVaRest_JsonType::JSON_String:
+			Type = K2Schema->PC_String;
+			break;
+		case EVaRest_JsonType::JSON_Object:
+			Type = K2Schema->PC_Object;
+			Subtype = Class;
+			break;
 		}
-
 		UEdGraphPin *OutputPin = CreatePin(EGPD_Output, Type, TEXT(""), Subtype, (*it).bIsArray, false, (*it).Name);
 	}
 }
@@ -274,3 +258,4 @@ FText UVaRest_BreakJson::GetNodeTitle(ENodeTitleType::Type TitleType) const
 {
 	return LOCTEXT("VaRest_Break_Json.NodeTitle", "Break Json");
 }
+
