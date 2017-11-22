642d3126575433d2500dd5fb47e40695a7629020
unktomi <christopher.d.oliver@gmail.com>
2017-11-22 18:32:48 +0000

    add make json node

--- Source/VaRestEditorPlugin/Classes/VaRest_BreakJson.h
+++ Source/VaRestEditorPlugin/Classes/VaRest_BreakJson.h
@@ -10,7 +10,6 @@
 #else
 #include "Engine.h"
 #endif
-
 #include "K2Node.h"
 
 #include "VaRest_BreakJson.generated.h"
@@ -39,6 +38,37 @@ struct FVaRest_NamedType
 	UPROPERTY(EditAnywhere, Category = NamedType)
 	bool bIsArray;
 };
+
+UCLASS(BlueprintType, Blueprintable)
+class VARESTEDITORPLUGIN_API UVaRest_MakeJson : public UK2Node
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+	// Begin UEdGraphNode interface.
+	virtual void AllocateDefaultPins() override;
+	virtual FLinearColor GetNodeTitleColor() const override;
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+	// End UEdGraphNode interface.
+
+	// Begin UK2Node interface
+	virtual bool IsNodePure() const { return true; }
+	virtual bool ShouldShowNodeProperties() const { return true; }
+	void GetMenuActions(FBlueprintActionDatabaseRegistrar& ActionRegistrar) const override;
+	virtual FText GetMenuCategory() const override;
+	virtual FText GetNodeTitle(ENodeTitleType::Type TitleType) const override;
+	virtual class FNodeHandlingFunctor* CreateNodeHandler(class FKismetCompilerContext& CompilerContext) const override;
+	// End UK2Node interface.
+
+protected:
+	virtual void CreateProjectionPins(UEdGraphPin *Source);
+
+public:
+	UPROPERTY(EditAnywhere, Category = PinOptions)
+	TArray<FVaRest_NamedType> Inputs;
+
+};
+
 
 UCLASS(BlueprintType, Blueprintable)
 class VARESTEDITORPLUGIN_API UVaRest_BreakJson : public UK2Node
