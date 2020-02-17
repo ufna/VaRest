2e282ac228b333c794774924d877435a98e40e72
Russ Treadwell <git@trdwll.com>
2020-02-17 05:29:13 -0500

    Move construction methods from VaRestJsonValue to VaRestSubsystem and update CoreRedirects.

--- Source/VaRest/Private/VaRestSubsystem.cpp
+++ Source/VaRest/Private/VaRestSubsystem.cpp
@@ -5,6 +5,7 @@
 #include "VaRestDefines.h"
 #include "VaRestJsonObject.h"
 #include "VaRestSettings.h"
+#include "VaRestJsonValue.h"
 
 #include "Developer/Settings/Public/ISettingsModule.h"
 #include "Misc/FileHelper.h"
@@ -105,6 +106,73 @@ UVaRestRequestJSON* UVaRestSubsystem::ConstructVaRestRequestExt(ERequestVerb Ver
 UVaRestJsonObject* UVaRestSubsystem::ConstructVaRestJsonObject()
 {
 	return NewObject<UVaRestJsonObject>(this);
+}
+
+UVaRestJsonValue* UVaRestSubsystem::ConstructJsonValueNumber(float Number)
+{
+	TSharedPtr<FJsonValue> NewVal = MakeShareable(new FJsonValueNumber(Number));
+
+	UVaRestJsonValue* NewValue = NewObject<UVaRestJsonValue>();
+	NewValue->SetRootValue(NewVal);
+
+	return NewValue;
+}
+
+UVaRestJsonValue* UVaRestSubsystem::ConstructJsonValueString(const FString& StringValue)
+{
+	TSharedPtr<FJsonValue> NewVal = MakeShareable(new FJsonValueString(StringValue));
+
+	UVaRestJsonValue* NewValue = NewObject<UVaRestJsonValue>();
+	NewValue->SetRootValue(NewVal);
+
+	return NewValue;
+}
+
+UVaRestJsonValue* UVaRestSubsystem::ConstructJsonValueBool(bool InValue)
+{
+	TSharedPtr<FJsonValue> NewVal = MakeShareable(new FJsonValueBoolean(InValue));
+
+	UVaRestJsonValue* NewValue = NewObject<UVaRestJsonValue>();
+	NewValue->SetRootValue(NewVal);
+
+	return NewValue;
+}
+
+UVaRestJsonValue* UVaRestSubsystem::ConstructJsonValueArray(const TArray<UVaRestJsonValue*>& InArray)
+{
+	// Prepare data array to create new value
+	TArray<TSharedPtr<FJsonValue>> ValueArray;
+	for (auto InVal : InArray)
+	{
+		ValueArray.Add(InVal->GetRootValue());
+	}
+
+	TSharedPtr<FJsonValue> NewVal = MakeShareable(new FJsonValueArray(ValueArray));
+
+	UVaRestJsonValue* NewValue = NewObject<UVaRestJsonValue>();
+	NewValue->SetRootValue(NewVal);
+
+	return NewValue;
+}
+
+UVaRestJsonValue* UVaRestSubsystem::ConstructJsonValueObject(UVaRestJsonObject* JsonObject)
+{
+	TSharedPtr<FJsonValue> NewVal = MakeShareable(new FJsonValueObject(JsonObject->GetRootObject()));
+
+	UVaRestJsonValue* NewValue = NewObject<UVaRestJsonValue>();
+	NewValue->SetRootValue(NewVal);
+
+	return NewValue;
+}
+
+UVaRestJsonValue* UVaRestSubsystem::ConstructJsonValue(const TSharedPtr<FJsonValue>& InValue)
+{
+	TSharedPtr<FJsonValue> NewVal = InValue;
+
+	UVaRestJsonValue* NewValue = NewObject<UVaRestJsonValue>();
+	NewValue->SetRootValue(NewVal);
+
+	return NewValue;
 }
 
 class UVaRestJsonObject* UVaRestSubsystem::LoadJsonFromFile(const FString& Path, const bool bIsRelativeToContentDir)
