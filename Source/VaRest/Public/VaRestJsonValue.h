ecc336aa3853a10155b077b49bfa300dbf6c38b7
Vladimir Alyamkin <ufna@ufna.ru>
2021-01-15 17:35:35 +0300

    Make getters pure

--- Source/VaRest/Public/VaRestJsonValue.h
+++ Source/VaRest/Public/VaRestJsonValue.h
@@ -40,36 +40,36 @@ class VAREST_API UVaRestJsonValue : public UObject
 	// FJsonValue API
 
 	/** Get type of Json value (Enum) */
-	UFUNCTION(BlueprintCallable, Category = "VaRest|Json")
+	UFUNCTION(BlueprintPure, Category = "VaRest|Json")
 	EVaJson GetType() const;
 
 	/** Get type of Json value (String) */
-	UFUNCTION(BlueprintCallable, Category = "VaRest|Json")
+	UFUNCTION(BlueprintPure, Category = "VaRest|Json")
 	FString GetTypeString() const;
 
 	/** Returns true if this value is a 'null' */
-	UFUNCTION(BlueprintCallable, Category = "VaRest|Json")
+	UFUNCTION(BlueprintPure, Category = "VaRest|Json")
 	bool IsNull() const;
 
 	/** Returns this value as a double, throwing an error if this is not an Json Number
 	 * Attn.!! float used instead of double to make the function blueprintable! */
-	UFUNCTION(BlueprintCallable, Category = "VaRest|Json")
+	UFUNCTION(BlueprintPure, Category = "VaRest|Json")
 	float AsNumber() const;
 
 	/** Returns this value as a int32, throwing an error if this is not an Json Number */
-	UFUNCTION(BlueprintCallable, Category = "VaRest|Json")
+	UFUNCTION(BlueprintPure, Category = "VaRest|Json")
 	int32 AsInt32() const;
 
 	/** Returns this value as a int64, throwing an error if this is not an Json Number */
-	UFUNCTION(BlueprintCallable, Category = "VaRest|Json")
+	UFUNCTION(BlueprintPure, Category = "VaRest|Json")
 	int32 AsInt64() const;
 
 	/** Returns this value as a string, throwing an error if this is not an Json String */
-	UFUNCTION(BlueprintCallable, Category = "VaRest|Json")
+	UFUNCTION(BlueprintPure, Category = "VaRest|Json")
 	FString AsString() const;
 
 	/** Returns this value as a boolean, throwing an error if this is not an Json Bool */
-	UFUNCTION(BlueprintCallable, Category = "VaRest|Json")
+	UFUNCTION(BlueprintPure, Category = "VaRest|Json")
 	bool AsBool() const;
 
 	/** Returns this value as an array, throwing an error if this is not an Json Array */
